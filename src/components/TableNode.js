13a101f8e7d00b62569363fd95b8facdd3047a28
Kevin Ghadyani <Saturn2888@gmail.com>
2019-03-17 09:28:46 -0500

    Moved TableNode styles to CSS

--- src/components/TableNode.js
+++ src/components/TableNode.js
@@ -2,18 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const TableNode = ({
-	color,
 	id,
 	value,
 }) => (
 	<td
+		className="TableNode"
 		id={`cell-${id}`}
-		style={{
-			padding: 0,
-			margin: 0,
-			lineHeight: '0.8em',
-			color,
-		}}
 	>
 		{value}
 	</td>
